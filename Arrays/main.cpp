9e0bf6e2b7a1209878de60e81198a5756acf33b9
Luis Fernando <luis_rogu@outlook.com>
2023-10-30 20:42:58 +0100

    How to init variables and arrays

---
+++ Arrays/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int8_t testScores_u8 [5] = {100, 95, 99, 87, 88};
+    int8_t highScorePerPlayer_u8 [10] = {3,5};
+    const int16_t daysInYear_u16 = 365;
+    int16_t  temperatures [daysInYear_u16] = {0};
+
+    printf("This is an example on how to initialize and use arrays");
+    return 0;
+}
