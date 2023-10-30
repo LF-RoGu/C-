e8176ef9cb77d9cd0f189d0dd802462bd0653a2c
Luis Fernando <luis_rogu@outlook.com>
2023-10-30 22:16:08 +0100

    add another example on how use pointers

--- Pointers/src/main.cpp
+++ Pointers/src/main.cpp
@@ -7,17 +7,28 @@ int main()
     int8_t highTemperature_f = 100;
     int8_t lowTemperature_f = 4;
 
-    /* Null ptr to set to addr 0 */
-    int8_t *ptr_temperature = nullptr;
     /* Set that ptr temperature is pointing to the value of highTemperature */
-    ptr_temperature = &highTemperature_f;
+    int8_t *ptr_highTemperature = &highTemperature_f;
+
+    /* Set that ptr temperature is pointing to the value of highTemperature */
+    int8_t *ptr_lowTemperature = nullptr;
+    ptr_lowTemperature = &lowTemperature_f;
+
 
     /* PTR is not pointing anywhere */
-    printf("Value of ptr_temperature... %x\n", ptr_temperature);
+    printf("Value of ptr_highTemperature... %x\n", ptr_highTemperature);
     /* Value that it is pointing at */
-    printf("Value of *ptr_temperature... %d\n", *ptr_temperature);
+    printf("Value of *ptr_highTemperature... %d\n", *ptr_highTemperature);
     /* Address wher value is being stored */
-    printf("Value of &ptr_temperature... %x\n", &ptr_temperature);
+    printf("Value of &ptr_highTemperature... %x\n", &ptr_highTemperature);
+
+
+    /* PTR is not pointing anywhere */
+    printf("Value of ptr_lowTemperature... %x\n", ptr_lowTemperature);
+    /* Value that it is pointing at */
+    printf("Value of *ptr_lowTemperature... %d\n", *ptr_lowTemperature);
+    /* Address wher value is being stored */
+    printf("Value of &ptr_lowTemperature... %x\n", &ptr_lowTemperature);
 
     return 0;
 }
