6f9f739ee99368d3c94bbc76d1ee84c3f5561829
Luis Fernando <luis_rogu@outlook.com>
2023-10-30 20:51:47 +0100

    Using vectors instead of arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
@@ -7,6 +10,12 @@ int main()
     const int16_t daysInYear_u16 = 365;
     int16_t  temperatures [daysInYear_u16] = {0};
 
-    printf("This is an example on how to initialize and use arrays");
+    printf("This is an example on how to initialize and use arrays \n");
+
+    /* Sufix '_v' for vector type */
+    vector <int> vowels_v {'a', 'e', 'i', 'o', 'u'};
+    printf("Vector at position %d\n"
+           "Letter in position... %c", 0, vowels_v.at(0));
+
     return 0;
 }
